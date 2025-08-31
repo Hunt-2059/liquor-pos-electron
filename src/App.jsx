a9984d251f3fba6642d6b5fe58f072bf15d5c2f6
Jason Ndirangu <jasonndirangu093@gmail.com>
2025-08-31 18:07:21 +0300

    Initial commit - Liquor POS Electron project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">Liquor Store POS</h1>
+      <p className="mt-4">Welcome! This is your Electron + React POS system.</p>
+    </div>
+  )
+}
+
+export default App
